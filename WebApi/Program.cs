755f87f57277a03dd1b3168a379eaf88cdbc18dc
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2025-09-09 19:11:57 +0200

    now

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -6,10 +6,13 @@ builder.Services.AddOpenApi();
 var app = builder.Build();
 app.MapOpenApi();
 
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/hi", () => "hi there");
+
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/hi", () => "hi there");
+
 app.Run();
